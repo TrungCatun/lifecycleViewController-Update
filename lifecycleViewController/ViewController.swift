ad3b7afcceae8d712d8ebc7c90d9becb338e30bf
Trung <trung@iMac-cua-Admin.local>
2018-06-15 09:45:54 +0700

    Initial Commit

---
+++ lifecycleViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lifecycleViewController
+//
+//  Created by Trung on 6/15/18.
+//  Copyright © 2018 TrungCatun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
