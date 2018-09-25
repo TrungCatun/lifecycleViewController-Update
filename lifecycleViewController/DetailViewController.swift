b268293518eb7760d97ab978a0a0928cfd761c96
Trung <>
2018-09-25 15:17:34 +0700

    Update vong doi viewcontroller

--- lifecycleViewController/DetailViewController.swift
+++ lifecycleViewController/DetailViewController.swift
@@ -1,50 +1,47 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  lifecycleViewController
 //
-//  Created by Trung on 6/15/18.
+//  Created by Trung on 9/25/18.
 //  Copyright © 2018 TrungCatun. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("view did load in ViewController a \n")
-        // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad in DetailViewController 1 \n")
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("view will Appear in viewController b \n")
+        print("view will Appear in DetailViewController 2 \n")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("viewDidAppear in viewController c \n")
+        print("viewDidAppear in DetailViewController 3 \n")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-         print("viewWillDisappear in viewController d \n")
+        print("viewWillDisappear in DetailViewController 4 \n")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("viewDidDisappear in viewController e \n")
+        print("viewDidDisappear in DetailViewController 5 \n")
     }
     
     deinit {
-        print("\(type(of: self)) - deinit f \n")
+        print("\(type(of: self)) - deinit 6 \n")
         
     }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
